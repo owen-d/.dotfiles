246286a6a06381246bf0e3f40705d3737448191f
owen-d <ow.diehl@gmail.com>
2017-05-06 23:25:57 -0400

    basic spacemacs configs

---
+++ spacemacs-layers/owend-dotfile/layers.el
@@ -0,0 +1,9 @@
+;;;; List of personal layers
+(configuration-layer/declare-layers '(
+                                      git
+                                      markdown
+                                      clojure
+                                      javascript
+                                      themes-megapack
+                                      rust
+                                      ))
---
+++ spacemacs-layers/owend-dotfile/user-config-dropin.el
@@ -0,0 +1,3 @@
+(spacemacs/toggle-smartparens-globally-on)
+(setq-default js2-basic-offset 2)
+(setq-default js-indent-level 2)
