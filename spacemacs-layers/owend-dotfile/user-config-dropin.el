5b36d53a0c1c07558c4a87999334d2d6ae53a40d
Owen Diehl <owen@fugue.co>
2017-11-07 17:32:40 -0500

    manually loads ludwig-mode

--- spacemacs-layers/owend-dotfile/user-config-dropin.el
+++ spacemacs-layers/owend-dotfile/user-config-dropin.el
@@ -1,3 +1,7 @@
 (spacemacs/toggle-smartparens-globally-on)
 (setq-default js2-basic-offset 2)
 (setq-default js-indent-level 2)
+;; ludwig mode cloned into ~/lib/emacs from https://github.com/fugue/ludwig-mode
+(add-to-list 'load-path "~/lib/emacs/ludwig-mode/")
+(autoload 'ludwig-mode "ludwig-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.lw\\'" . ludwig-mode))
