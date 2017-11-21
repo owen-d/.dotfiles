8902c0d2390f76282672ba5efb49789904f323b3
Owen Diehl <owen@fugue.co>
2017-11-21 10:48:15 -0500

    pbcopy/paste from emacs when osx

--- spacemacs-layers/owend-dotfile/user-config-dropin.el
+++ spacemacs-layers/owend-dotfile/user-config-dropin.el
@@ -8,3 +8,17 @@
 
 ;; org mode posthook
 (add-hook 'org-mode-hook (lambda () (visual-line-mode 1)))
+
+;; osx system copy/paste integration from terminal
+(defun paste-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun copy-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+        (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+            (process-send-string proc text)
+            (process-send-eof proc))))
+
+(when (eq system-type 'darwin)
+  (setq interprogram-cut-function 'copy-to-osx)
+  (setq interprogram-paste-function 'paste-from-osx))
