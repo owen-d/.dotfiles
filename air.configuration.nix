86818a8a59406f7ecbe581d682871d49830b5f77
Owen Diehl <ow.diehl@gmail.com>
2021-01-23 15:47:03 +0000

    begins rig nixos

---
+++ air.configuration.nix
@@ -0,0 +1,173 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+
+  boot.loader.systemd-boot.enable = true;
+  # Whether or not the installation process should modify EFI boot variables
+  boot.loader.efi.canTouchEfiVariables = true;
+  # If you rely on a dirty /tmp dir you are doing it wrong. Your laptop will
+  # never be cattle.
+  boot.cleanTmpDir = true;
+  # This gets your audio output and input (mic) working
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda2";
+      preLVM = true;
+      allowDiscards = true;
+    }
+  ];
+
+
+  #networking.hostName = "nix"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    vim
+    emacs
+    git
+    google-chrome
+    vlc
+    curl wget
+    tmux
+    zip unzip
+    xcape
+    xclip
+    streamlink
+    alacritty
+    lsof
+    powertop
+    stack
+    gnumake
+    gcc
+    binutils
+    clang
+  ];
+
+  systemd.services.powertop = {
+    description = "Power Management tunings";
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      ${pkgs.powertop}/bin/powertop --auto-tune
+      ${pkgs.iw}/bin/iw dev wlp3s0 set power_save on
+      for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+      echo powersave > $cpu
+      done
+    '';
+    serviceConfig.Type = "oneshot";
+  };
+
+
+  systemd.user.services."xcape" = {
+    enable = true;
+    description = "xcape to use CTRL as ESC when pressed alone";
+    wantedBy = [ "default.target" ];
+    serviceConfig.Type = "forking";
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = 2;
+    serviceConfig.ExecStartPre = "${pkgs.xorg.setxkbmap}/bin/setxkbmap -option ctrl:nocaps -option caps:ctrl_modifier";
+    serviceConfig.ExecStart = "${pkgs.xcape}/bin/xcape -e Control_L=Escape";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.bash.enableCompletion = true;
+  programs.man.enable = true;
+  programs.mosh.enable = true;
+  programs.ssh.startAgent = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.tlp.enable = true;
+  services.thermald.enable = true;
+  # unclutter to hide mouse while not in use
+  services.unclutter.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbVariant = "mac";
+
+  services.xserver.displayManager.auto = {
+    enable = true;
+    user = "owen";
+  };
+
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.naturalScrolling = true;
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.owen = {
+    extraGroups = ["wheel"];
+    isNormalUser = true;
+    uid = 1000;
+  };
+
+  security.sudo.configFile = ''
+  # Don't edit this file. Set the NixOS options ‘security.sudo.configFile’
+  # or ‘security.sudo.extraConfig’ instead.
+
+  # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
+  Defaults env_keep+=SSH_AUTH_SOCK
+
+  # "root" is allowed to do anything.
+  root        ALL=(ALL:ALL) SETENV: ALL
+
+  # Users in the "wheel" group can do anything.
+  %wheel      ALL=(ALL:ALL) NOPASSWD:SETENV: ALL
+
+  # Keep terminfo database for root and %wheel.
+  Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+  Defaults:root,%wheel env_keep+=TERMINFO
+
+  '';
+
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;    ## If compatibility with 32-bit applications is desired.
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09"; # Did you read the comment?
+
+}
+
+
--- configuration.nix
+++ configuration.nix
@@ -10,71 +10,43 @@
       ./hardware-configuration.nix
     ];
 
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   nixpkgs.config = {
     allowUnfree = true;
   };
 
-
-  boot.loader.systemd-boot.enable = true;
-  # Whether or not the installation process should modify EFI boot variables
-  boot.loader.efi.canTouchEfiVariables = true;
-  # If you rely on a dirty /tmp dir you are doing it wrong. Your laptop will
-  # never be cattle.
-  boot.cleanTmpDir = true;
-  # This gets your audio output and input (mic) working
-  boot.initrd.luks.devices = [
-    {
-      name = "root";
-      device = "/dev/sda2";
-      preLVM = true;
-      allowDiscards = true;
-    }
-  ];
-
-
-  #networking.hostName = "nix"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  # time.timeZone = "Europe/Amsterdam";
 
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0.useDHCP = true;
 
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    vim
-    emacs
-    git
-    google-chrome
-    vlc
-    curl wget
-    tmux
-    zip unzip
-    xcape
-    xclip
-    streamlink
-    alacritty
-    lsof
-    powertop
-    stack
-    gnumake
-    gcc
-    binutils
-    clang
-  ];
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  systemd.services.powertop = {
-    description = "Power Management tunings";
-    wantedBy = [ "multi-user.target" ];
-    script = ''
-      ${pkgs.powertop}/bin/powertop --auto-tune
-      ${pkgs.iw}/bin/iw dev wlp3s0 set power_save on
-      for cpu in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
-      echo powersave > $cpu
-      done
-    '';
-    serviceConfig.Type = "oneshot";
-  };
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.autoRepeatDelay = 185;
+  services.xserver.autoRepeatInterval = 50;
 
 
   systemd.user.services."xcape" = {
@@ -87,24 +59,65 @@
     serviceConfig.ExecStartPre = "${pkgs.xorg.setxkbmap}/bin/setxkbmap -option ctrl:nocaps -option caps:ctrl_modifier";
     serviceConfig.ExecStart = "${pkgs.xcape}/bin/xcape -e Control_L=Escape";
   };
+  
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.owen = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    emacs
+    git
+    google-chrome
+    vlc
+    curl
+    wget
+    tmux
+    zip unzip
+    xcape
+    xclip
+    streamlink
+    alacritty
+    lsof
+    gnumake
+    gcc
+    binutils
+    clang
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  programs.bash.enableCompletion = true;
-  programs.man.enable = true;
-  programs.mosh.enable = true;
-  programs.ssh.startAgent = true;
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.tlp.enable = true;
-  services.thermald.enable = true;
-  # unclutter to hide mouse while not in use
-  services.unclutter.enable = true;
+  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -112,62 +125,13 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.layout = "us";
-  services.xserver.xkbVariant = "mac";
-
-  services.xserver.displayManager.auto = {
-    enable = true;
-    user = "owen";
-  };
-
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Enable touchpad support.
-  services.xserver.libinput.enable = true;
-  services.xserver.libinput.naturalScrolling = true;
-
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.extraUsers.owen = {
-    extraGroups = ["wheel"];
-    isNormalUser = true;
-    uid = 1000;
-  };
-
-  security.sudo.configFile = ''
-  # Don't edit this file. Set the NixOS options ‘security.sudo.configFile’
-  # or ‘security.sudo.extraConfig’ instead.
-
-  # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
-  Defaults env_keep+=SSH_AUTH_SOCK
-
-  # "root" is allowed to do anything.
-  root        ALL=(ALL:ALL) SETENV: ALL
-
-  # Users in the "wheel" group can do anything.
-  %wheel      ALL=(ALL:ALL) NOPASSWD:SETENV: ALL
-
-  # Keep terminfo database for root and %wheel.
-  Defaults:root,%wheel env_keep+=TERMINFO_DIRS
-  Defaults:root,%wheel env_keep+=TERMINFO
-
-  '';
-
-  hardware.pulseaudio.enable = true;
-  hardware.pulseaudio.support32Bit = true;    ## If compatibility with 32-bit applications is desired.
-
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "17.09"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
 
 }
-
 
