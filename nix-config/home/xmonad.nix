b5d51d84f97bbbe7ebca2f9da89e6a3bf309407a
Owen Diehl <ow.diehl@gmail.com>
2021-02-01 20:55:53 -0500

    xmonad spacing

--- nix-config/home/xmonad.nix
+++ nix-config/home/xmonad.nix
@@ -1,8 +1,11 @@
 ''
   -- owen-d's XMonad config
   -- This file is managed by NixOS, don't edit it directly!
+  -- lovingly crafted from https://github.com/fortuneteller2k/nix-config/blob/master/nixos/config/xmonad.nix
   import XMonad
+
   import XMonad.Actions.CycleWS
+
   import XMonad.Hooks.DynamicLog
   import XMonad.Hooks.EwmhDesktops
   import XMonad.Hooks.InsertPosition
@@ -10,6 +13,8 @@
   import XMonad.Hooks.ManageHelpers
   import XMonad.Hooks.Place
   import XMonad.Hooks.SetWMName
+
+  import XMonad.Layout.DraggingVisualizer
   import XMonad.Layout.Grid
   import XMonad.Layout.NoBorders
   import XMonad.Layout.Renamed
@@ -18,21 +23,27 @@
   import XMonad.Layout.Tabbed
   import XMonad.Layout.ThreeColumns
   import XMonad.Layout.ToggleLayouts
+
   import XMonad.Prompt
   import XMonad.Prompt.FuzzyMatch
   import XMonad.Prompt.Shell
+
   import XMonad.Util.EZConfig
   import XMonad.Util.Run
   import XMonad.Util.SpawnOnce
+
   import Data.Char
   import Data.Monoid
+
   import System.IO
   import System.Exit
+
   import qualified DBus                     as D
   import qualified DBus.Client              as D
   import qualified Codec.Binary.UTF8.String as UTF8
   import qualified XMonad.StackSet          as W
   import qualified Data.Map                 as M
+
 
   -- mod key - use super (windows key)
   modkey = mod4Mask
@@ -44,13 +55,7 @@
   keybindings =
     [ ("M-<Return>",                 spawn "alacritty")
     , ("M-q",                        kill)
-    , ("M--",                        sendMessage Shrink)
-    , ("M-=",                        sendMessage Expand)
-    , ("C-<Left>",                   prevWS)
-    , ("C-<Right>",                  nextWS)
     , ("M-S-r",                      spawn $ "xmonad --restart && systemctl --user restart polybar")
-    , ("M-S-<Left>",                 shiftToPrev >> prevWS)
-    , ("M-S-<Right>",                shiftToNext >> nextWS)
     ]
     ++
     [ (otherModMasks ++ "M-" ++ key, action tag)
@@ -58,6 +63,17 @@
         , (otherModMasks, action) <- [ ("", windows . W.greedyView)
                                      , ("S-", windows . W.shift)]
     ]
+
+  layouts = avoidStruts $ tiled ||| mtiled ||| tabs ||| centeredMaster ||| grid
+    where
+       tiled = stripName 2 0 $ gaps 4 4 $ draggingVisualizer $ toggleLayouts maximized (smartBorders (Tall 1 (3/100) (1/2)))
+       mtiled = stripName 2 0 $ gaps 4 4 $ draggingVisualizer $ Mirror (toggleLayouts maximized (smartBorders (Tall 1 (3/100) (1/2))))
+       centeredMaster = stripName 2 0 $ gaps 4 4 $ draggingVisualizer $ toggleLayouts maximized (smartBorders (ThreeColMid 1 (3/100) (1/2)))
+       tabs = stripName 1 1 $ gaps 8 0 $ noBorders (tabbed shrinkText tabTheme)
+       grid = stripName 2 0 $ gaps 4 4 $ draggingVisualizer $ toggleLayouts maximized (smartBorders Grid)
+       maximized = smartBorders Full
+       gaps n k = spacingRaw False (Border n n n n) True (Border k k k k) True
+       stripName n k = renamed [Chain [CutWordsLeft n, CutWordsRight k]]
 
   tabTheme = def
     { fontName            = fontFamily
@@ -110,6 +126,7 @@
     , workspaces         = ws
     , normalBorderColor  = "#2e303e"
     , focusedBorderColor = "#e95678"
+    , layoutHook         = showWName' wnameTheme layouts
     , logHook            = polybarHook dbus
     , startupHook        = autostart
     }
